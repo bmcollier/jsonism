8afeac7c3fd3216ea0fed1e3c78be22b735ced70
Ben Collier <bencollier@fastmail.com>
2022-03-09 09:58:17 +0000

    Version increase

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='jsonism',
-    version='1.0.7',
+    version='1.0.8',
     packages=['tests', 'jsonism'],
     url='https://github.com/bmcollier/jsonism',
     license='3-Clause BSD',
