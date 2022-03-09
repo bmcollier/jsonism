61cb42b4ecf9720ad5e1bdfcb6f7aa4656674a4b
Ben Collier <bencollier@fastmail.com>
2022-03-09 08:04:28 +0000

    Adding markdown to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='jsonism',
-    version='1.0.2',
+    version='1.0.3',
     packages=['tests', 'jsonism'],
     url='https://github.com/bmcollier/jsonism',
     license='3-Clause BSD',
     author='Ben Collier',
     author_email='bencollier@fastmail.com',
-    description='A simple JSON schema checker'
+    description='A simple JSON schema checker',
+    long_description_content_type="text/markdown"
 )
